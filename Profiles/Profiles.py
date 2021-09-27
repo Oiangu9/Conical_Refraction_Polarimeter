Problems with the new modus operandi solved. Solved also a big problem in the correct initialilzation of the point triads. New scripts made for plotting image profiles, image averages etc. Everything for the report to Dr. Kumar Utukuri.

Profiles/Profiles.py
@@ -7,7 +7,7 @@ from mpl_toolkits.mplot3d import Axes3D
 import imageio
 
 
-main_path="/home/oiangu/Desktop/Conical_Refraction_Polarimeter/Profiles/Polarizer/"
+main_path="/home/oiangu/Desktop/Conical_Refraction_Polarimeter/Profiles/test3/"
 
 path_dict={}
 for (dirpath, dirnames, filenames) in os.walk(main_path):
@@ -58,7 +58,7 @@ for dirpath, filenames in path_dict.items():
             ax = fig.add_subplot(224, projection='3d')
             X,Y = np.meshgrid(np.arange(len(prof_x)),np.arange(len(prof_y)))
             im=images[filename].transpose()
-            fig.suptitle(f"Intesity Profiles for Image {filename}")
+            fig.suptitle(f"Intesity Profiles for Image\n{filename}")
             files_for_gif=[]
             cbax=fig.add_axes([0.54,0.05,0.4,0.01])
             fig.colorbar(cm, ax=axes[0,0], cax=cbax, orientation='horizontal')
@@ -73,7 +73,7 @@ for dirpath, filenames in path_dict.items():
                 ax.set_ylabel('Z')
                 #ax.set_ylim(-10, 8)
                 ax.set_zlabel('Intensity')
-                ax.set_zlim(-20, np.max(im))
+                ax.set_zlim(-0.078*np.max(im), np.max(im))
                 ax.set_title("Image intensity 3D plot")
                 ax.view_init(10, theta)
                 ax.get_proj = lambda: np.dot(Axes3D.get_proj(ax), np.diag([1.3, 1.3, 1.3, 1]))

SOURCE/Ad_Hoc_Optimizer.py
@@ -161,15 +161,25 @@ class Ad_Hoc_Optimizer:
         # Evaluate cost function for each angle
         #print("active_xs",active_xs, "costos", [ self.evaluate_cost(image, angle, *args_for_cost) for angle in active_xs])
         active_points = np.stack((active_xs, [ self.evaluate_cost(image, angle, *args_for_cost) for angle in active_xs])) # [2 (xj,f(xj)),4]
-        # if the minium is in the boundary of the interval, make it not be the boundary. we subtract the initial_guess_delta to break the symmetries
-        if np.argmin(active_points[1])==0:
-            active_points[0, 3] -= 3*(self.b-self.a)/2+self.initial_guess_delta
-            active_points[1,3] = self.evaluate_cost(image, active_points[0, 3], *args_for_cost)
-        elif np.argmin(active_points[1])==3:
-            active_points[0, 0] += 3*(self.b-self.a)/2+self.initial_guess_delta
-            active_points[1,0] = self.evaluate_cost(image, active_points[0,0], *args_for_cost)
+        # if the minium is in the boundary of the interval, make it not be the boundary. we make the interval slightly wider by 10% of the original interval and a little random factor to prevent strange symmetries
+        patience=10 # try first moving the inside candidates -> make it a tunable parameter!!
+        while(np.argmin(active_points[1])==0 or np.argmin(active_points[1])==3):
+            if patience>0:# random gittering for the inside points without getting outside the boundaries
+                active_points[0, 1] += (2*np.random.randint(0,2)-1)*np.random.rand(1)[0]*min(active_points[0, 1]-self.a, self.b-active_points[0, 1])*0.9 # add/subtract randomly a random factor of the distance from the point to the closest boundary
+                active_points[0,2] += (2*np.random.randint(0,2)-1)*np.random.rand(1)[0]*min(active_points[0, 2]-self.a, self.b-active_points[0, 2])*0.9 # the 0.91 is to avoid the point to fall on the boundary if the randmo gittering takes value 1
+                active_points[1,1] = self.evaluate_cost(image, active_points[0, 1], *args_for_cost)
+                active_points[1,2] = self.evaluate_cost(image, active_points[0, 2], *args_for_cost)
 
-        # order the four pairs of points by their support position
+                patience-=1
+            else: # make the boundaries themselves bigger - note this is not the preferred approach but might be compulsory for some algorithms
+                if np.argmin(active_points[1])==0:
+                    active_points[0, 0] -= (0.25*(self.b-self.a)+self.initial_guess_delta*np.random.rand(1)[0])
+                    active_points[1,0] = self.evaluate_cost(image, active_points[0, 0], *args_for_cost)
+                if np.argmin(active_points[1])==3:
+                    active_points[0, 3] += (0.25*(self.b-self.a)+self.initial_guess_delta*np.random.rand(1)[0])
+                    active_points[1,3] = self.evaluate_cost(image, active_points[0,3], *args_for_cost)
+
+        # order the four pairs of points by their support position (not anymore! they should laready be ordered!) active_points[:, np.argsort(active_points[0])]
         return active_points[:, np.argsort(active_points[0])]
 
     def quadratic_fit_search(self, precision, max_iterations, cost_tol, image, args_for_cost, initial_guess=None):

SOURCE/GPU_Classes.py
@@ -197,8 +197,8 @@ class RingSimulator_Optimizer_GPU():
 
     def compute_and_plot_CR_ring(self, CR_ring_angle, R0_pixels, Z, R0, out_path, name):
         I=self.compute_CR_ring(CR_ring_angle, R0_pixels, Z, R0)
-        cv2.imwrite(f"{out_path}/[{name}]__PolAngle_{CR_ring_angle/2:.15f}_CRAngle_{CR_ring_angle:.15f}_Z_{Z}_R0_{R0}_L_{(self.nx-1)/2.0/R0_pixels}.png",
-                (65534*I).astype(np.uint16))
+        cv2.imwrite(f"{out_path}/[{name}]__PolAngle_{CR_ring_angle/2:.15f}_CRAngle_{CR_ring_angle:.15f}_Z_{Z}_R0_{R0}_R0_pix_{R0_pixels}.png",
+                (65534*I).astype(np.uint16)) # L=(self.nx-1)/2.0/R0_pixels
 
 
 
@@ -206,7 +206,7 @@ if __name__ == "__main__":
 
     #phi_CRs = [-3, -2, np.pi/2, -1, 0, 1, np.pi/2, 2, 3, np.pi]
     #phi_CRs = [-2.3932]
-    phi_CRs=[2.977385307179586]
+    phi_CRs=[-2.3932,  2.977317734726711] # such that 2*26.146deg is their difference
 
     '''
     print("\n\n\nTesting General Simulator:")
@@ -227,13 +227,34 @@ if __name__ == "__main__":
     '''
 
     print("\n\n\nTesting Optimizer Simulator:")
-    simulator=RingSimulator_Optimizer_GPU( n=1.5, w0=1, a0=1.0, max_k=50, num_k=1200, nx=1215, sim_chunk_x=607, sim_chunk_y=607)
+    simulator=RingSimulator_Optimizer_GPU( n=1.5, w0=1, a0=1.0, max_k=50, num_k=1200, nx=1101, sim_chunk_x=550, sim_chunk_y=550)
 
-    os.makedirs('./Simulated/Optimizer/Full/', exist_ok=True)
+    os.makedirs('./Simulated/Optimizer/Full/SIMULATED_Kumar_Example/1101_Resolution', exist_ok=True)
     os.makedirs('./Simulated/Optimizer/Approx/', exist_ok=True)
 
     for phi_CR in phi_CRs:
         print(f"Computing {phi_CR}")
-        simulator.compute_and_plot_CR_ring( phi_CR, R0_pixels=237.79, Z=0, R0=23.25, out_path='./Simulated/Optimizer/Full/', name='')
+        simulator.compute_and_plot_CR_ring( phi_CR, R0_pixels=237.79, Z=0, R0=30.25, out_path='./Simulated/Optimizer/Full/SIMULATED_Kumar_Example/1101_Resolution/', name='Big_Radious_Very_Thin_Width_')
+        simulator.compute_and_plot_CR_ring( phi_CR, R0_pixels=237.79, Z=0, R0=23.25, out_path='./Simulated/Optimizer/Full/SIMULATED_Kumar_Example/1101_Resolution/', name='Big_Radious_Thin_Width_')
+        simulator.compute_and_plot_CR_ring( phi_CR, R0_pixels=180.7, Z=0, R0=23.25, out_path='./Simulated/Optimizer/Full/SIMULATED_Kumar_Example/1101_Resolution/', name='Small_Radious_Thin_Width_')
+        simulator.compute_and_plot_CR_ring( phi_CR, R0_pixels=120.7, Z=0, R0=23.25, out_path='./Simulated/Optimizer/Full/SIMULATED_Kumar_Example/1101_Resolution/', name='Very_Small_Radious_Thin_Width_')
+        simulator.compute_and_plot_CR_ring( phi_CR, R0_pixels=237.7, Z=0, R0=15.25, out_path='./Simulated/Optimizer/Full/SIMULATED_Kumar_Example/1101_Resolution/', name='Big_Radious_Thick_Width_')
+        simulator.compute_and_plot_CR_ring( phi_CR, R0_pixels=237.7, Z=0, R0=10.25, out_path='./Simulated/Optimizer/Full/SIMULATED_Kumar_Example/1101_Resolution/', name='Big_Radious_Very_Thick_Width_')
+        print("Hard one done!\n")
+        #simulator.compute_intensity_Todor_and_Plot(phi_CR/2, './Simulated/Optimizer/Approx/')
+
+    simulator=RingSimulator_Optimizer_GPU( n=1.5, w0=1, a0=1.0, max_k=50, num_k=1200, nx=2202, sim_chunk_x=550, sim_chunk_y=550)
+
+    os.makedirs('./Simulated/Optimizer/Full/SIMULATED_Kumar_Example/2202_Resolution/', exist_ok=True)
+    os.makedirs('./Simulated/Optimizer/Approx/', exist_ok=True)
+
+    for phi_CR in phi_CRs:
+        print(f"Computing {phi_CR}")
+        simulator.compute_and_plot_CR_ring( phi_CR, R0_pixels=475.58, Z=0, R0=30.25, out_path='./Simulated/Optimizer/Full/SIMULATED_Kumar_Example/2202_Resolution/', name='Big_Radious_Very_Thin_Width_')
+        simulator.compute_and_plot_CR_ring( phi_CR, R0_pixels=475.58, Z=0, R0=23.25, out_path='./Simulated/Optimizer/Full/SIMULATED_Kumar_Example/2202_Resolution/', name='Big_Radious_Thin_Width_')
+        simulator.compute_and_plot_CR_ring( phi_CR, R0_pixels=361.4, Z=0, R0=23.25, out_path='./Simulated/Optimizer/Full/SIMULATED_Kumar_Example/2202_Resolution/', name='Small_Radious_Thin_Width_')
+        simulator.compute_and_plot_CR_ring( phi_CR, R0_pixels=241.4, Z=0, R0=23.25, out_path='./Simulated/Optimizer/Full/SIMULATED_Kumar_Example/2202_Resolution/', name='Very_Small_Radious_Thin_Width_')
+        simulator.compute_and_plot_CR_ring( phi_CR, R0_pixels=475.58, Z=0, R0=15.25, out_path='./Simulated/Optimizer/Full/SIMULATED_Kumar_Example/2202_Resolution/', name='Big_Radious_Thick_Width_')
+        simulator.compute_and_plot_CR_ring( phi_CR, R0_pixels=475.58, Z=0, R0=10.25, out_path='./Simulated/Optimizer/Full/SIMULATED_Kumar_Example/2202_Resolution/', name='Big_Radious_Very_Thick_Width_')
         print("Hard one done!\n")
         #simulator.compute_intensity_Todor_and_Plot(phi_CR/2, './Simulated/Optimizer/Approx/')

SOURCE/Image_Manager.py
@@ -260,12 +260,13 @@ class Image_Manager:
             self.centered_images_to_plot=self.centered_ring_images.copy()
         for im, (name, angle) in enumerate(pol_angles.items()):
             # Note that the image will be permanently modified!
+            maxI=np.max(self.centered_images_to_plot[im])
             cv2.putText(self.centered_images_to_plot[im],
                 f"{angle} +-{precisions[name]} {unit}", # text to insert
                 (10,30), # spot in the image
                 cv2.FONT_HERSHEY_SIMPLEX, # font
                 1, # font scale
-                (255,0,0), # font color,
+                (int(maxI),0,0), # font color,
                 2) # line type
             if show:
                 self.mainThreadPlotter.emit(self.centered_images_to_plot[im],