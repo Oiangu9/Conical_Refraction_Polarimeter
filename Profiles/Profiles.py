0775b603a1486468da02a7187fe4eb06985d8e3e
Xabier Oyanguren Asua <oiangu9@gmail.com>
2021-10-20 15:55:04 +0200

    Meeting of20/10/2021, second meeting, new images!

--- Profiles/Profiles.py
+++ Profiles/Profiles.py
@@ -6,8 +6,8 @@ import os
 from mpl_toolkits.mplot3d import Axes3D
 import imageio
 
-
-main_path="/home/oiangu/Desktop/Conical_Refraction_Polarimeter/DATA/EXPERIMENTAL/Utukuri_et_al/2__28_09_2021/Some_i607/Profiles/2"
+main_path="/home/melanie/Desktop/Conical_Refraction_Polarimeter/Experimental_Stuff/dia2/laser_gaussian_thesis"
+#main_path="/home/oiangu/Desktop/Conical_Refraction_Polarimeter/DATA/EXPERIMENTAL/Utukuri_et_al/2__28_09_2021/Some_i607/Profiles/2"
 #main_path="/home/oiangu/Desktop/Conical_Refraction_Polarimeter/DATA/EXPERIMENTAL/Utukuri_et_al/3__29_09_2021/i607/Profiles"
 #main_path="/home/oiangu/Desktop/Conical_Refraction_Polarimeter/DATA/EXPERIMENTAL/Utukuri_et_al/4__01_10_2021/Profiles"
 path_dict={}
